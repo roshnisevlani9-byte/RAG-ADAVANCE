3bfffa76b859f39bd3dd148b0a218d9d65ddd880
roshnisevlani9-byte <roshnisevlani9@gmail.com>
2026-07-25 15:10:47 +0530

    Update prompt.py
    gugjk

--- prompt.py
+++ prompt.py
@@ -1,1 +1,18 @@
 
+PROMPT = """
+You are an AI assistant.
+
+Answer only using the provided context.
+
+If the answer is not in the context, say
+
+"I don't know based on the uploaded document."
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
